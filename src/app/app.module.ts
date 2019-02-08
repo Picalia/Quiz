28ce60924ed08f7a6de64482917445ed3fe26276
Andres Cardenas <andrescr2503@gmail.com>
2019-02-08 09:29:45 -0600

    Terminé proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,3 +30,5 @@ import { HomePage } from '../pages/home/home';
   ]
 })
 export class AppModule {}
+
+// En el 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { stringify } from '@angular/core/src/util';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  casas = [];
+
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.http.get("/v1/api/pin-data?url=/s-renta-inmuebles/guadalajara-y-zona-metro/v1c1098l10567p1&geo=(21.10631012145462,-102.42214381725364),(20.21712862656199,-104.32387728274637)")
+    .subscribe(data => {
+      console.log(JSON.stringify(data));
+      if (data.hasOwnProperty('ads'))
+      this.casas= data['ads'];
+
+    }, error => {
+      console.log(JSON.stringify(error));
+
+    });
+
 
   }
 
 }
+
+// Imagen se encuentra  dentro de ads - en el arreglo de picture- en URL 
+// Precio se encuentra dentro de ads - en el diccionario price - en formattedAmount
+// Ubicacion se encuentra; Ciudad: en el diccionario geo - en name
+// Colonia: en el diccionario geo - en displayName
+
+
